89d0e2eb9ca2742a2c1b6bb58531d107f9cbab93
pako8101 <pako810129@gmail.com>
2024-03-07 16:40:30 +0200

    routs add

--- src/main/java/com/example/myroutdiscover/web/RouteController.java
+++ src/main/java/com/example/myroutdiscover/web/RouteController.java
@@ -1,12 +1,17 @@
 package com.example.myroutdiscover.web;
 
+import com.example.myroutdiscover.model.binding.RouteAddBindingModel;
 import com.example.myroutdiscover.model.view.RouteViewModel;
 import com.example.myroutdiscover.service.RouteService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -14,31 +19,44 @@ import java.util.List;
 @RequestMapping("/routes")
 public class RouteController {
 
-private final RouteService routeService;
+    private final RouteService routeService;
 
     public RouteController(RouteService routeService) {
         this.routeService = routeService;
     }
 
+    @ModelAttribute
+    public RouteAddBindingModel routeAddBindingModel() {
+        return new RouteAddBindingModel();
+    }
+
     @GetMapping("/all")
-    public String allRoutes(Model model){
+    public String allRoutes(Model model) {
 
         List<RouteViewModel> routeViewModelList = routeService.findAllRoutesView();
-model.addAttribute("routes",routeViewModelList);
+        model.addAttribute("routes", routeViewModelList);
 
 
         return "routes";
     }
 
     @GetMapping("/add")
-    public String add(){
+    public String add() {
 
 
         return "add-route";
     }
-//    @PostMapping("/")
-//    public String Routes(){
-//        return "routes";
-//    }
+    @PostMapping("/add")
+    public String addConfirm(@Valid RouteAddBindingModel routeAddBindingModel,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes){
+
+        if (bindingResult.hasErrors()){
+
+        }
+
+
+        return "redirect:all";
+    }
 
 }
